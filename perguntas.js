7dd19a4fa63e0e9048ddbc789d99cd073d3e4eef
Drika7 <adriana.lima.silva@escola.pr.gov.br>
2024-11-13 11:26:48 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,35 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Música',
+    'Qual foi o primeiro grupo de kpop a ser indicado (duas vezes) ao grammy?',
+    'BTS (Bangtan Sonyedan/Bangtan Boys).'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Mundo',
+    'Qual grupo musical foi escolhido para dicurssar na ONU e em que ano isso aconteceu?',
+    'BTS, no ano de 2021.'
 )
 
 criaCartao(
-    'Programação',
+    'Música',
+    'Qual foi o primeiro artista do Kpop a ultrapassar mais de um bilhão de streams em três de suas músicas?',
+    'Jung-kook (Jeon Jungkook), com Seven ft. Latto,Left e Right e Standing Next to You.
+)
+
+criaCartao(
+    'BTS',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+
+criaCartao(
+    'BTS',
     'O que é uma função?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
-    'Lingua inglesa',
+    'BTS',
     'Como se diz oi em Inglês?',
     'Oi em ingles é HI (RAI)'
 )
