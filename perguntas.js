7eb12ec640ce5ed61f60a5e057e589d886c0652d
Drika7 <adriana.lima.silva@escola.pr.gov.br>
2024-12-04 11:27:44 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -6,8 +6,8 @@ criaCartao(
 
 criaCartao(
     'Mundo',
-    'Qual grupo musical foi escolhido para dicurssar na ONU e em que ano isso aconteceu?',
-    'BTS, no ano de 2021.'
+    'Em que ano o grupo musical "BTS" foi escolhido para dicurssar e performar na ONU?',
+    '2021.'
 )
 
 criaCartao(
@@ -31,5 +31,151 @@ criaCartao(
 
 criaCartao(
     'BTS',
-    '
+    'Qual foi o primeiro prêmio internacional que o BTS ganhou?'
+    'Mnet Asian Music Awards (2015)'
 )
+criaCartao(
+    'Música',
+    'Em que ano o BTS fez sua primeira aparição no Billboard Music Awards?',
+    '2017'
+)
+
+criaCartao(
+    'Mundo',
+    'Qual foi a primeira música do BTS a alcançar 1 bilhão de visualizações no YouTube?',
+    'Fake Love.'
+)
+
+criaCartao(
+    'Música',
+    'Em que ano o BTS foi indicado ao Grammy pela primeira vez?',
+    '2020'
+)
+
+criaCartao(
+    'BTS',
+    'Qual álbum do BTS foi o primeiro a alcançar o topo da Billboard 200?'
+    'Map of the Soul: 7'
+    
+)
+
+criaCartao(
+    'BTS',
+    'Qual foi a primeira turnê mundial do BTS?'
+    '2014 BTS Live Trilogy – Episode II: The Red Bullet'
+)
+
+criaCartao(
+    'BTS',
+    'Em que ano o BTS ganhou o prêmio de "Melhor Grupo" MAMA?'
+    '2016'
+)
+criaCartao(
+    'Música',
+    'Qual música do BTS foi a primeira a entrar no Top 10 da Billboard Hot 100?',
+    'DNA'
+)
+
+criaCartao(
+    'Mundo',
+    'Quantos prêmioS MTV Video Music Awards o BTS tem?',
+    '10 prêmios.'
+)
+
+criaCartao(
+    'Música',
+    'Qual foi o album mais vendido do BTS até hoje?',
+    'WINGS'
+)
+
+criaCartao(
+    'BTS',
+    'Quantos álbuns do BTS já alcançaram o primeiro lugar nas paradas do Japão?'
+    '4'
+    
+)
+
+criaCartao(
+    'BTS',
+    'Em que ano o BTS começou a se apresentar no Melon Music Awards?'
+    '2019'
+)
+
+criaCartao(
+    'BTS',
+    'O BTS já fez história nas paradas de vendas de álbuns nos Estados Unidos. Qual álbum deles entrou no Top 10 da Billboard 200?'
+    'Be'
+)
+criaCartao(
+    'Música',
+    'Qual foi o primeiro show do BTS no Brasil?',
+    '2014'
+)
+
+criaCartao(
+    'Mundo',
+    'Quantas vezes o BTS veio para o Brasil',
+    '4.'
+)
+
+criaCartao(
+    'Música',
+    'Como se chama o fandom oficial do BTS?',
+    'ARMY'
+)
+
+criaCartao(
+    'BTS',
+    'Qual membro do BTS tem uma habilidade especial para tocar o piano?'
+    'Min Yoongi (Suga)'
+    
+)
+
+criaCartao(
+    'BTS',
+    'Quais membros do BTS tem uma paixão por fotografia e frequentemente compartilha suas fotos nas redes sociais?'
+    'Jungkook (Jeon Jungkook) e V (Kim Taehyung)'
+)
+
+criaCartao(
+    'BTS',
+    'Qual membro do BTS tem o apelido de "Golden Maknae"?'
+    'Jungkook (Jeon Jungkook)'
+)
+criaCartao(
+    'Música',
+    'Qual é o nome do primeiro single em inglês do BTS?',
+    'Dynamite'
+)
+
+criaCartao(
+    'Mundo',
+    'Qual membro do BTS tem um talento especial para cozinhar?',
+    'Jin (Kim SeokJin)'
+)
+
+criaCartao(
+    'Música',
+    'Em que ano o BTS lançou o álbum Love Yourself: Tear?'
+    '2018'
+)
+
+criaCartao(
+    'BTS',
+    'Qual é o nome do documentário sobre o BTS que foi lançado nos cinemas em 2019?'
+    'Bring The Soul: The Movie'
+    
+)
+
+criaCartao(
+    'BTS',
+    'RM é fluente em 5 idiomas. Quais?'
+    'Coreano,inglês e japones'
+)
+
+criaCartao(
+    'BTS',
+    'Qual o verdadeiro nome do membro J-hope?'
+    'Jung Hoseok'
+)
+
